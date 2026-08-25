624e31e624ed167af4fd17c7d37433115b91b979
Tarundeep Singh <tdeep4355@gmail.com>
2026-08-26 00:13:19 +0530

    fix(announcement): fix alembic migration down_revision to avoid multiple heads conflict

--- backend/alembic/versions/aa0fc0d8d7a4_add_send_email_to_announcements.py
+++ backend/alembic/versions/aa0fc0d8d7a4_add_send_email_to_announcements.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'aa0fc0d8d7a4'
-down_revision: Union[str, None] = '0092'
+down_revision: Union[str, None] = '0093'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
